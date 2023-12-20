8afb5db12c673683f266f4941b48c2de73ae53f2
yongseok.choi <dydtjr1994@gmail.com>
2023-12-20 17:26:16 +0900

    change more dark random colors

--- chzzk.js
+++ chzzk.js
@@ -19,13 +19,41 @@
     const randomColorNumber =  20;
 
     function 랜덤색상생성() {
-        const 최소값 = 50; // 어두운 색을 피하기 위한 최소값
-        const 최대값 = 200; // 너무 밝은 색을 피하기 위한 최대값
+      const 색상 = Math.floor(Math.random() * 360); // 0부터 360까지의 색상 값
+      const 채도 = Math.floor(Math.random() * 50) + 50; // 50부터 100까지의 채도 값 (더 진한 색)
+      const 명도 = Math.floor(Math.random() * 40) + 50; // 50부터 90까지의 명도 값 (어두운 색)
 
-        const 랜덤RGB값 = () => Math.floor(Math.random() * (최대값 - 최소값 + 1)) + 최소값;
+      // HSL을 RGB로 변환
+      const rgb색상 = hslToRgb(색상 / 360, 채도 / 100, 명도 / 100);
 
-        const 랜덤색상 = `rgb(${랜덤RGB값()}, ${랜덤RGB값()}, ${랜덤RGB값()})`;
-        return 랜덤색상;
+      return `rgb(${rgb색상[0]}, ${rgb색상[1]}, ${rgb색상[2]})`;
+    }
+
+    // HSL을 RGB로 변환하는 함수
+    function hslToRgb(h, s, l){
+        let r, g, b;
+
+        if(s === 0){
+            r = g = b = l; // 그레이 스케일
+        } else {
+            const hue2rgb = (p, q, t) => {
+                if(t < 0) t += 1;
+                if(t > 1) t -= 1;
+                if(t < 1/6) return p + (q - p) * 6 * t;
+                if(t < 1/2) return q;
+                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                return p;
+            };
+
+            const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            const p = 2 * l - q;
+
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+
+        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
     }
 
     function 텍스트를_숫자로_변환(text) {
